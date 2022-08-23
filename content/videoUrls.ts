80581e0a08dcafd37714552609be914d7a578801
Jacob Silver <jsilver@tempo.io>
2022-08-23 08:08:48 -0400

    add full video links

--- content/videoUrls.ts
+++ content/videoUrls.ts
@@ -3,32 +3,148 @@ interface VideoUrl {
   title: string;
 }
 export const videoUrls: VideoUrl[] = [
-  { title: "My Sweet Lord", url: "s6F74JYOrEM" },
-  { title: "Chained", url: "Kl24lPhZPvA" },
-  { title: "Ain't No Justice", url: "3QB4RGnyQus" },
-  { title: "Gathering Memories", url: "mgC7jB3bLzI" },
-  { title: "Wichita Lineman", url: "eq4pE6zCDbQ" },
+  {
+    title: "Faithful Man - with Lee Fields",
+    url: "https://www.youtube.com/watch?v=fNSjGC-T80s",
+  },
+  {
+    title: "Tryin' Times - with Gail Ann Dorsey",
+    url: "https://www.youtube.com/watch?v=wDDa3yrfrkw",
+  },
+  {
+    title: "Lovin' You, Baby - with Charles Bradley",
+    url: "https://www.youtube.com/watch?v=bORgsmSNhfA",
+  },
+  {
+    title: "Lay My Burden Down - with Aoife O'Donovan",
+    url: "https://www.youtube.com/watch?v=UuXb4She_sU",
+  },
+  {
+    title:
+      "It Makes No Difference - with Lucinda Williams and The Midnight Ramble Band",
+    url: "https://www.youtube.com/watch?v=jd6rKp8HMVc",
+  },
+  {
+    title:
+      "Further On Up The Road - with Bob Weir and The Midnight Ramble Band",
+    url: "https://www.youtube.com/watch?v=3VOPtA-oRY0",
+  },
+  {
+    title:
+      "Up On Cripple Creek - with Buddy Miller and The Midnight Ramble Band",
+    url: "https://www.youtube.com/watch?v=JmQTTY4YAks",
+  },
+  {
+    title: "No Room At The Inn - with Catherine Russell",
+    url: "https://vimeo.com/143535593/2dfa78bc39?embedded=true&source=video_title&owner=2639578",
+  },
+  {
+    title:
+      "Won't Get Fooled Again - with Jimmy Vivino, Amy Helm and Rich Pagano",
+    url: "https://www.youtube.com/watch?v=ht31UsO351c",
+  },
+  {
+    title: "When The Sun Comes Around - with The Mike + Ruthy Band",
+    url: "https://www.youtube.com/watch?v=IWqANRko2fE",
+  },
+  {
+    title: "This Christmas - with Simi Stone",
+    url: "https://vimeo.com/132623110/dc796dbff6?embedded=true&source=video_title&owner=2639578",
+  },
+  {
+    title:
+      "Christmas Must Be Tonight - with Amy Helm, Connor Kennedy and Catherine Russell",
+    url: "https://vimeo.com/139492659/2ff710cf15?embedded=true&source=video_title&owner=2639578",
+  },
+  {
+    title: "My Sweet Lord",
+    url: "https://www.youtube.com/watch?v=s6F74JYOrEM",
+  },
+  {
+    title: "Chained",
+    url: "https://www.youtube.com/watch?v=Kl24lPhZPvA&t=19s",
+  },
+  {
+    title: "Ain't No Justice",
+    url: "https://www.youtube.com/watch?v=3QB4RGnyQus",
+  },
+  {
+    title: "Gathering Memories",
+    url: "https://www.youtube.com/watch?v=mgC7jB3bLzI",
+  },
+  {
+    title: "Wichita Lineman",
+    url: "https://www.youtube.com/watch?v=eq4pE6zCDbQ",
+  },
   {
     title: "Running Back And Forth",
-    url: "2iNOY-zrz-k",
+    url: "https://www.youtube.com/watch?v=2iNOY-zrz-k",
   },
-  { title: "Sinebar", url: "8jod37aX-dQ" },
-  { title: "Heartaches", url: "tjuhSBIyR-E" },
-  { title: "Loving You Is Sweeter Than Ever", url: "VuDbyy0j5-w " },
-  { title: "You Gave Me Love", url: "qeC1szsD8vI " },
-  { title: "Shoe Leather Expressway", url: "7m_N90MgN-c" },
-  { title: "If You Let Me", url: "LdqGV6ok_N8" },
-  { title: "I'm In A Different World", url: "bmMjh3eTS9g" },
-  { title: "Baby Don't You Do It", url: "GU4v20XepPw" },
-  { title: "Every Time I See You I Go Wild", url: "GzumIAWeYv4" },
-  { title: "I'd Cry", url: "5MRriRwbtlk" },
-  { title: "Send Me Some Lovin", url: "3hUmltcMKgA" },
-  { title: "World of Darkness", url: "uXlrubEeMgQ" },
-  { title: "Joy Takes Over Me", url: "HpdJk81wMV8" },
-  { title: "Bach Violin Sonata No  1 in G Minor: Presto", url: "yZ3hgvReusg" },
-  { title: "House On The Hill", url: "ZGfG8m7zBk4" },
-  { title: "Bach Cello Suite #1 prelude", url: "76SBvcBvb8w" },
-  { title: "Anything You Want Me To Do", url: "9KptS-EJKwE" },
-  { title: "Phil Cunningham Set on electric bass", url: "ej_cfuZm1ak" },
-  { title: "For Once In My Life", url: "sm43bd5Bv1s" },
+  { title: "Sinebar", url: "https://www.youtube.com/watch?v=8jod37aX-dQ" },
+  { title: "Heartaches", url: "https://www.youtube.com/watch?v=tjuhSBIyR-E" },
+  {
+    title: "Loving You Is Sweeter Than Ever",
+    url: "https://www.youtube.com/watch?v=VuDbyy0j5-w",
+  },
+  {
+    title: "You Gave Me Love",
+    url: "https://www.youtube.com/watch?v=qeC1szsD8vI",
+  },
+  {
+    title: "Shoe Leather Expressway",
+    url: "https://www.youtube.com/watch?v=7m_N90MgN-c",
+  },
+  {
+    title: "If You Let Me",
+    url: "https://www.youtube.com/watch?v=LdqGV6ok_N8",
+  },
+  {
+    title: "I'm In A Different World",
+    url: "https://www.youtube.com/watch?v=bmMjh3eTS9g",
+  },
+  {
+    title: "Baby Don't You Do It",
+    url: "https://www.youtube.com/watch?v=GU4v20XepPw",
+  },
+  {
+    title: "Every Time I See You I Go Wild",
+    url: "https://www.youtube.com/watch?v=GzumIAWeYv4",
+  },
+  { title: "I'd Cry", url: "https://www.youtube.com/watch?v=5MRriRwbtlk" },
+  {
+    title: "Send Me Some Lovin",
+    url: "https://www.youtube.com/watch?v=3hUmltcMKgA",
+  },
+  {
+    title: "World of Darkness",
+    url: "https://www.youtube.com/watch?v=uXlrubEeMgQ",
+  },
+  {
+    title: "Joy Takes Over Me",
+    url: "https://www.youtube.com/watch?v=HpdJk81wMV8",
+  },
+  {
+    title: "Bach Violin Sonata No  1 in G Minor: Presto",
+    url: "https://www.youtube.com/watch?v=yZ3hgvReusg",
+  },
+  {
+    title: "House On The Hill",
+    url: "https://www.youtube.com/watch?v=ZGfG8m7zBk4",
+  },
+  {
+    title: "Bach Cello Suite #1 prelude",
+    url: "https://www.youtube.com/watch?v=76SBvcBvb8w",
+  },
+  {
+    title: "Anything You Want Me To Do",
+    url: "https://www.youtube.com/watch?v=9KptS-EJKwE",
+  },
+  {
+    title: "Phil Cunningham Set on electric bass",
+    url: "https://www.youtube.com/watch?v=ej_cfuZm1ak",
+  },
+  {
+    title: "For Once In My Life",
+    url: "https://www.youtube.com/watch?v=sm43bd5Bv1s",
+  },
 ];
