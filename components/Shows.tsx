b820ee0e788030a68cd92b49db2e3783f0f966ef
Jacob Silver <jsilver@tempo.io>
2022-09-16 16:01:18 -0400

    filter shows in the past

--- components/Shows.tsx
+++ components/Shows.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Head from "next/head";
 import { shows } from "../content";
+import { isFuture, parse } from "date-fns";
 
 const ShowsContainer = styled.div`
   text-align: left;
@@ -17,26 +18,39 @@ const ShowContainer = styled.div`
   }
 `;
 
-export const ShowsComponent = () => (
-  <>
-    <Head>
-      <title>Jacob Silver - Shows</title>
-      <meta name="description" content="Jacob Silver Homepage" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <ShowsContainer>
-      {shows.map((show, index) => (
-        <ShowContainer key={index}>
-          <a href={show.ticketLink} target="_blank" rel="noopener noreferrer">
-            <h1>{show.date}</h1>
-            <p>{show.band}</p>
-            <p>{show.venue}</p>
-            <p>
-              {show.city}, {show.state}, {show.country}
-            </p>
-          </a>
-        </ShowContainer>
-      ))}
-    </ShowsContainer>
-  </>
-);
+export const ShowsComponent = () => {
+  const filteredShows = shows.filter((show) => {
+    const date = parse(show.date, "M/d/yyyy", new Date());
+    return isFuture(date);
+  });
+
+  return (
+    <>
+      <Head>
+        <title>Jacob Silver - Shows</title>
+        <meta name="description" content="Jacob Silver Homepage" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ShowsContainer>
+        {filteredShows.map((show, index) => {
+          return (
+            <ShowContainer key={index}>
+              <a
+                href={show.ticketLink}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h1>{show.date}</h1>
+                <p>{show.band}</p>
+                <p>{show.venue}</p>
+                <p>
+                  {show.city}, {show.state}, {show.country}
+                </p>
+              </a>
+            </ShowContainer>
+          );
+        })}
+      </ShowsContainer>
+    </>
+  );
+};
